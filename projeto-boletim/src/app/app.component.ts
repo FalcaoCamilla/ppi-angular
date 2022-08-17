21ba09034f50d013bea350f1c2231fb450903750
FalcaoCamilla <millafalcao.acad@gmail.com>
2022-08-17 11:54:49 -0300

    help student v1.1

--- projeto-boletim/src/app/app.component.ts
+++ projeto-boletim/src/app/app.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `<app-media></app-media>`
 })
 export class AppComponent {
   title = 'projeto-boletim';
--- projeto-boletim/src/app/media/media.component.ts
+++ projeto-boletim/src/app/media/media.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit } from '@angular/core';
 import { Boletim } from '../model/boletim';
 
 
@@ -7,24 +7,26 @@ import { Boletim } from '../model/boletim';
   templateUrl: './media.component.html',
   styleUrls: ['./media.component.css']
 })
-export class MediaComponent implements OnInit {
+export class MediaComponent implements OnInit{
   boletim: Boletim;
   media: number;
   mensagem: string;
-  visible: boolean;
+  visible: boolean = true;
+  changeText: boolean;
 
   constructor() {
     this.boletim = new Boletim()
     this.media = 0;
     this.mensagem = '';
-    this.visible = false;
+    this.changeText = false;
   }
 
   ngOnInit(): void {
   }
 
   calcular() {
+    this.visible = false;
     this.media = this.boletim.calcularMedia();
-    this.mensagem = `Sua media final é: ${this.media}`
+    this.mensagem = `Sua media final é ${this.media}`
   }
 }
